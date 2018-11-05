46f05471284bda0931241dddbf17a4078f8e2711
AlexZhao <zhaozixuan0729@126.com>
2018-11-05 17:59:08 -0600

    adding hashmap for standard form

--- ar4ds/core.py
+++ ar4ds/core.py
@@ -3,8 +3,12 @@ This module defines the core language
 and logic api.
 '''
 from .dc import *
+import re
 
 def compile(rule="True", pre="True"):
+    print(rule)
+    print(pre)
+    print(eval('lambda s,t: '+rule))
     return DC(eval('lambda s,t: ' + rule), \
               eval('lambda s,t: ' + pre), \
               rule, pre)
@@ -59,3 +63,65 @@ def _check(*args):
     for arg in args:
         if not isinstance(arg, bool):
             AssertionError("The argument " + arg + " is not a boolean")
+
+
+def is_standard_form(dc):
+    rule = dc.rexp
+    if rule[:7] == "implies":
+        cntp = 0
+        split_point = 7
+        for c in rule[8:]:
+            if c == "(" :
+                cntp += 1
+            elif c == ")":
+                cntp -= 1
+            if cntp == 0 and c == ",":
+                break
+            split_point += 1
+        a,b = rule[7:split_point], rule[split_point+1:]
+        if "disj" in a or "xor" in a or "iff" in a or "implies" in a:
+            return False
+        cnt = a.count("eq") + a.count("ne") + a.count("ge") + a.count("le")
+        if cnt != 1:
+            return False
+    return True
+
+def hashmap_standard_form(dc, data):
+    rule = dc.rexp
+    rule = rule.replace(" ", "")
+    
+    mp = dict()
+    header = list(data.columns.values)
+    for item in header:
+        mp[item] = dict()
+
+    for index, row in data.iterrows():
+        for item in header:
+            if row[item] not in mp[item]: 
+                mp[item][row[item]] = []
+            mp[item][row[item]].append(index)
+    # print("hash_map:",mp)
+    # mp is mapping name of different columns to different sub-hashmaps where values are hashed by their corresponding keys
+
+
+    if rule[:7] == "implies":
+        cntp = 0
+        split_point = 7
+        for c in rule[8:]:
+            if c == "(" :
+                cntp += 1
+            elif c == ")":
+                cntp -= 1
+            if cntp == 0 and c == ",":
+                break
+            split_point += 1
+        a = rule[7:split_point+1].split("eq")
+
+    for equality in a[1:]:
+        temp = equality[1: -2].split(",")
+        compared_col1, compared_col2 = temp[0].split(".")[1], temp[1].split(".")[1]
+        # compared_col1 and compared_col2 are the columns needed to compare in each of the equality expressions
+
+
+        
+
