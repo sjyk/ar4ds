46b5f969c06ddea03b3f245954539dfcb06580f8
amyvzeng <amyzeng.am@gmail.com>
2018-12-01 20:18:55 -0600

    Rewrote cdfjoin plotter wrappers, cleaned loadimbdata

--- ar4ds/cdfjoin/loadimdbdata.py
+++ ar4ds/cdfjoin/loadimdbdata.py
@@ -70,91 +70,72 @@ csv_names = {"aka_name": aka_name, "aka_title": aka_title, "cast_info": cast_inf
 # In[4]:
 
 
-csv_tables = {}
-
-for csv_title in csv_names.keys():
-    with open('./imdb/' + csv_title + '.csv', newline = '') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        table = []
-        headers = csv_names[csv_title]
-        counter = 0
-        for row in spamreader:
-            if counter >= 1000:
-                break
-            else:
-                counter += 1
-                table.append({headers[i]: row[i] for i in range(len(headers))})
-    csv_tables[csv_title] = table
+join_attr_info = []
 
 
 # In[5]:
 
 
-with open('edited-queries.txt', 'r') as f:
-    content = f.readlines()
+def read_imdb_data():
+    imdb_csv_data = {}
     
-table_info = []
-join_attr_info = []
-
-line_ctr=0
-for line in content:
-    if line != "\n":
-        line = line.strip().strip(';')
-        if line_ctr % 2 == 0:
-            new_tables = line.split(", ")
-            table_info.extend([table for table in new_tables if table not in table_info])
-        else:
-            new_attrs = line.split(" AND ")
-            join_attr_info.extend([attr for attr in new_attrs if attr not in join_attr_info])
-        line_ctr+=1
+    for csv_title in csv_names.keys():
+        with open('./imdb/' + csv_title + '.csv', newline = '') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',')
+            table = []
+            headers = csv_names[csv_title]
+            counter = 0
+            for row in spamreader:
+                if counter >= 1000:
+                    break
+                else:
+                    counter += 1
+                    table.append({headers[i]: row[i] for i in range(len(headers))})
+        imdb_csv_data[csv_title] = table
         
-tables = {}
-for table in table_info:
-    info = table.split(" AS ")
-    tables[info[1]] = [deepcopy(entry) for entry in csv_tables[info[0]]]
+    return imdb_csv_data
 
 
 # In[6]:
 
 
-# def join_tables_on_key(t1, t2, t1_key, t2_key):
-#     table = []
-#     for entry1 in t1:
-#         for entry2 in t2:
-#             if entry1[t1_key] == entry2[t2_key]:
-#                 entry = entry1
-#                 if t1_key==t2_key:
-#                     for k,v in entry2.items():
-#                         if k != t2_key:
-#                             entry[k] = v
-#                 else:
-#                     entry.update(entry2)
-#                 table.append(entry)
-#     return table
+def read_queries():
+    table_query_nicknames = []
+    
+    with open('edited-queries.txt', 'r') as f:
+        content = f.readlines()
+        
+    line_ctr=0
+    for line in content:
+        if line != "\n":
+            line = line.strip().strip(';')
+            if line_ctr % 2 == 0:
+                new_tables = line.split(", ")
+                table_query_nicknames.extend([table for table in new_tables if table not in table_query_nicknames])
+            else:
+                new_attrs = line.split(" AND ")
+                join_attr_info.extend([attr for attr in new_attrs if attr not in join_attr_info])
+            line_ctr+=1
+            
+    return table_query_nicknames
 
 
 # In[7]:
 
 
-# def create_joined_tables():
-#     joined_tables = []
-#     for join_attr in join_attr_info:
-#         attrs = join_attr.split(" = ")
-#         join_1 = attrs[0].split(".")
-#         join_2 = attrs[1].split(".")
-#         table = join_tables_on_key(tables[join_1[0]].copy(), tables[join_2[0]].copy(), join_1[1], join_2[1])
-#         if table != []:
-#             joined_tables.append(table)
-#     return joined_tables
+def create_query_tables():
+    csv_data = read_imdb_data()
+    query_info = read_queries()
+    query_tables = {}
+    
+    for table in query_info:
+        info = table.split(" AS ")
+        query_tables[info[1]] = [deepcopy(entry) for entry in csv_data[info[0]]]
+        
+    return query_tables
 
 
 # In[8]:
-
-
-#final_tables = create_joined_tables()
-
-
-# In[9]:
 
 
 def insert_key(table, original_key):
@@ -163,7 +144,7 @@ def insert_key(table, original_key):
     return table
 
 
-# In[10]:
+# In[9]:
 
 
 def remove_key(table, keys):
@@ -173,17 +154,19 @@ def remove_key(table, keys):
     return table
 
 
-# In[11]:
+# In[10]:
 
 
 def create_tables_to_join():
+    query_tables = create_query_tables()
+    
     table_pairs = []
     for join_attr in join_attr_info:
         attrs = join_attr.split(" = ")
         join_1 = attrs[0].split(".")
         join_2 = attrs[1].split(".")
-        table_1 = deepcopy(tables[join_1[0]])
-        table_2 = deepcopy(tables[join_2[0]])
+        table_1 = deepcopy(query_tables[join_1[0]])
+        table_2 = deepcopy(query_tables[join_2[0]])
         common_attrs = [attr for attr in table_1[0].keys() if attr in table_2[0].keys()]
         common_attrs = [attr for attr in common_attrs if attr != join_1[1] and attr != join_2[1]]
         if join_1[1] != 'id':
@@ -193,38 +176,14 @@ def create_tables_to_join():
         table_1 = remove_key(table_1, common_attrs)
         table_2 = remove_key(table_2, common_attrs)
         table_pairs.append([table_1, table_2])
+        
     return table_pairs
 
 
-# In[12]:
+# In[11]:
 
 
 table_pairs = create_tables_to_join()
-
-
-# In[13]:
-
-
-# def attempts_at_joining(pairs):
-#     joined_table_indices = []
-#     for i in range(len(pairs)):
-#         table_1 = pairs[i][0]
-#         table_2 = pairs[i][1]
-#         if len(natjoin.natural_join([deepcopy(table_1), deepcopy(table_2)])) != 0:
-#             joined_table_indices.append(i)
-#     return joined_table_indices
-
-
-# In[14]:
-
-
-# joined_table_indices = attempts_at_joining(table_pairs)
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
